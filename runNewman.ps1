c98b1d9938852a46268c4ec852cb87db01bc17fc
Amy Gao <35817129+AmyGao18@users.noreply.github.com>
2018-04-20 17:38:16 -0400

    Update runNewman.ps1

--- runNewman.ps1
+++ runNewman.ps1
@@ -1,2 +1,1 @@
-cd C:\Users\gaoa\AppData\Roaming\npm\node_modules\newman\bin
-node newman run Trello-tests.postman_collection.json -e Trello-tests.postman_environment.json -x --delay-request 10 -r teamcity
+newman run Trello-tests.postman_collection.json -e Trello-tests.postman_environment.json -x --delay-request 10 -r teamcity
